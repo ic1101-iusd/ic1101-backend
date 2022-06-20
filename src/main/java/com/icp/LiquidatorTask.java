2f478a3e239464c9781579d0d655e943b89f8a21
Максим Богуненко <mbohunenko@itsmartflex.com>
2022-06-20 15:16:04 +0300

    Add try catch

--- src/main/java/com/icp/LiquidatorTask.java
+++ src/main/java/com/icp/LiquidatorTask.java
@@ -16,28 +16,31 @@ public class LiquidatorTask extends TimerTask {
     @Override
     public void run() {
         System.out.println("Task Run");
+        try {
+            final BigDecimal price = liquidatorService.getPrice();
+            final BigInteger lastPositionId = liquidatorService.getLastPositionId();
+            final BigInteger limit = valueOf(100);
 
-        final BigDecimal price = liquidatorService.getPrice();
-        final BigInteger lastPositionId = liquidatorService.getLastPositionId();
-        final BigInteger limit = valueOf(100);
+            for (int offset = 0; offset < lastPositionId.intValue(); offset += 100) {
 
-        for (int offset = 0; offset < lastPositionId.intValue(); offset += 100) {
-
-            List<SharedPosition> liquidatedPositions = liquidatorService.getPositions(limit, valueOf(offset))
-                    .stream()
-                    .filter(sharedPosition -> !sharedPosition.getDeleted())
-                    .filter(positionDTO -> liquidatorService.isLiquidated(positionDTO, price))
-                    .collect(Collectors.toList());
-            liquidatedPositions.forEach(liquidatedPosition -> {
-                System.out.println(format("Start to liquidate %s", liquidatedPosition));
-                try {
-                    liquidatorService.liquidate(liquidatedPosition.getId());
-                } catch (Exception e) {
-                    System.out.println(format("Can't liquidate %s", e.getMessage()));
-                }
-                System.out.println(format("Finish to liquidate %s", liquidatedPosition));
-            });
-            System.out.println(format("Task Finish, liquidatedPositions count %s", liquidatedPositions.size()));
+                List<SharedPosition> liquidatedPositions = liquidatorService.getPositions(limit, valueOf(offset))
+                        .stream()
+                        .filter(sharedPosition -> !sharedPosition.getDeleted())
+                        .filter(positionDTO -> liquidatorService.isLiquidated(positionDTO, price))
+                        .collect(Collectors.toList());
+                liquidatedPositions.forEach(liquidatedPosition -> {
+                    System.out.println(format("Start to liquidate %s", liquidatedPosition));
+                    try {
+                        liquidatorService.liquidate(liquidatedPosition.getId());
+                    } catch (Exception e) {
+                        System.out.println(format("Can't liquidate %s", e.getMessage()));
+                    }
+                    System.out.println(format("Finish to liquidate %s", liquidatedPosition));
+                });
+                System.out.println(format("Task Finish, liquidatedPositions count %s", liquidatedPositions.size()));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }

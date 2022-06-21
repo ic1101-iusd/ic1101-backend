6540be7c78b9e2261338a7dc0074afe01afd128e
Максим Богуненко <mbohunenko@itsmartflex.com>
2022-06-21 22:21:52 +0300

    Refactor

--- build.sh
+++ build.sh
@@ -7,8 +7,4 @@ docker run -it -e ICP_NETWORK='https://ic0.app' \
 -e INIT_APPROVE='false' \
 -e AMOUNT_BTC='1000000000000' \
 -e AMOUNT_TOKEN='10000' \
--e IDENTITY_KEY='-----BEGIN PRIVATE KEY-----
-MFMCAQEwBQYDK2VwBCIEIFJcLiYH5ym+iBhKnPH7SfsFcZ+TU2Td3dXAvWu5JOWV
-oSMDIQBOt7dEpggtoXAILmiyolxKlwLIX716miMTuQYyOYPEpA==
------END PRIVATE KEY-----'\
 -t icp-liquidator-g -p 8080:8080
